21cfbc8f7b3c4a015098714f9013e95f0863c989
Douglas <94dousch@gafe.molndal.se>
2021-11-03 15:54:31 +0100

    Feature, program loop

--- CICDCalculationUppgift/Program.cs
+++ CICDCalculationUppgift/Program.cs
@@ -6,133 +6,127 @@
     {
         static void Main(string[] args)
         {
+            bool run = true;
 
-            Console.Write("Enter first operator: ");
-            string op1 = Console.ReadLine();
-
-            Console.Write("Enter second operator: ");
-            string op2 = Console.ReadLine();
-
-            Console.Write("Enter first number: ");
-            string first1 = Console.ReadLine();
-            double first = Convert.ToDouble(first1);
-
-            Console.Write("Enter second number : ");
-            string second2 = Console.ReadLine();
-            double second = Convert.ToDouble(second2);
-
-            Console.Write("Enter third number: ");
-            string third3 = Console.ReadLine();
-            double third = Convert.ToDouble(third3);
-
-            if (op1 == "+" && op2 == "+")
+            while (run)
             {
-                double summa = first + second + third;
-                Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                Console.Clear();
+                Console.Write("Enter first operator: ");
+                string op1 = Console.ReadLine();
+
+                Console.Write("Enter second operator: ");
+                string op2 = Console.ReadLine();
+
+                Console.Write("Enter first number: ");
+                string first1 = Console.ReadLine();
+                double first = Convert.ToDouble(first1);
+
+                Console.Write("Enter second number : ");
+                string second2 = Console.ReadLine();
+                double second = Convert.ToDouble(second2);
+
+                Console.Write("Enter third number: ");
+                string third3 = Console.ReadLine();
+                double third = Convert.ToDouble(third3);
+
+                if (op1 == "+" && op2 == "+")
+                {
+                    double summa = first + second + third;
+                    Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                }
+
+                else if (op1 == "+" && op2 == "-")
+                {
+                    double summa = first + second - third;
+                    Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                }
+
+                else if (op1 == "+" && op2 == "/")
+                {
+                    double summa = (first + second) / third;
+                    Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                }
+
+                else if (op1 == "+" && op2 == "*")
+                {
+                    double summa = (first + second) * third;
+                    Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                }
+
+                else if (op1 == "*" && op2 == "-")
+                {
+                    double summa = first * second - third;
+                    Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                }
+
+                else if (op1 == "*" && op2 == "+")
+                {
+                    double summa = first * second + third;
+                    Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                }
+
+                else if (op1 == "*" && op2 == "*")
+                {
+                    double summa = first * second * third;
+                    Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                }
+
+                else if (op1 == "*" && op2 == "/")
+                {
+                    double summa = (first * second) / third;
+                    Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                }
+
+                else if (op1 == "-" && op2 == "-")
+                {
+                    double summa = first - second - third;
+                    Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                }
+
+                else if (op1 == "-" && op2 == "+")
+                {
+                    double summa = first - second + third;
+                    Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                }
+
+                else if (op1 == "-" && op2 == "*")
+                {
+                    double summa = (first - second) * third;
+                    Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                }
+
+                else if (op1 == "-" && op2 == "/")
+                {
+                    double summa = (first - second) / third;
+                    Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                }
+
+                else if (op1 == "/" && op2 == "/")
+                {
+                    double summa = first / second / third;
+                    Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                }
+
+                else if (op1 == "/" && op2 == "+")
+                {
+                    double summa = (first / second) + third;
+                    Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                }
+
+                else if (op1 == "/" && op2 == "-")
+                {
+                    double summa = (first / second) - third;
+                    Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                }
+
+                else if (op1 == "/" && op2 == "*")
+                {
+                    double summa = (first / second) * third;
+                    Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
+                }
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
             }
-
-            else if (op1 == "+" && op2 == "-")
-            {
-                double summa = first + second - third;
-                Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
-            }
-
-            else if (op1 == "+" && op2 == "/")
-            {
-                double summa = (first + second) / third;
-                Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
-            }
-
-            else if (op1 == "+" && op2 == "*")
-            {
-                double summa = (first + second) * third;
-                Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
-            }
-
-            else if (op1 == "*" && op2 == "-")
-            {
-                double summa = first * second - third;
-                Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
-            }
-
-            else if (op1 == "*" && op2 == "+")
-            {
-                double summa = first * second + third;
-                Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
-            }
-
-            else if (op1 == "*" && op2 == "*")
-            {
-                double summa = first * second * third;
-                Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
-            }
-
-            else if (op1 == "*" && op2 == "/")
-            {
-                double summa = (first * second) / third;
-                Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
-            }
-
-            else if (op1 == "-" && op2 == "-")
-            {
-                double summa = first - second - third;
-                Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
-            }
-
-            else if (op1 == "-" && op2 == "+")
-            {
-                double summa = first - second + third;
-                Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
-            }
-
-            else if (op1 == "-" && op2 == "*")
-            {
-                double summa = (first - second) * third;
-                Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
-            }
-
-            else if (op1 == "-" && op2 == "/")
-            {
-                double summa = (first - second) / third;
-                Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
-            }
-
-            else if (op1 == "/" && op2 == "/")
-            {
-                double summa = first / second / third;
-                Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
-            }
-
-            else if (op1 == "/" && op2 == "+")
-            {
-                double summa = (first / second) + third;
-                Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
-            }
-
-            else if (op1 == "/" && op2 == "-")
-            {
-                double summa = (first / second) - third;
-                Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
-            }
-
-            else if (op1 == "/" && op2 == "*")
-            {
-                double summa = (first / second) * third;
-                Console.WriteLine(first + op1 + second + op2 + third + "=" + summa);
-            }
-
-
-
-
-
-            Console.Read();
-
-
-
-
-
-
-
         }
     }
 }
